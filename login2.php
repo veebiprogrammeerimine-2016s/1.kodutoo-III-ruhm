7feefb3936e5348e6230aa94ac568c5187699ad1
alekbaht <alekbaht@greeny.cs.tlu.ee>
2016-09-28 21:06:07 +0300

    added mvp idee

---
+++ login2.php
@@ -0,0 +1,170 @@
+<?php 
+	require("../../config.php");
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+		
+	$signupEmailError = "";
+	$signupEmail = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		// kas oli tühi
+		if (empty ($_POST["signupEmail"])) {
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+			
+		} else {
+				
+			// kõik korras, email ei ole tühi ja on olemas
+			$signupEmail = $_POST["signupEmail"];
+		}
+		
+	}
+	
+	$signupPasswordError = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST["signupPassword"])) {
+		
+		// oli olemas, ehk keegi vajutas nuppu
+		// kas oli tühi
+		if (empty ($_POST["signupPassword"])) {
+			
+			//oli tõesti tühi
+			$signupPasswordError = "See väli on kohustuslik";
+			
+		} else {
+			
+			// oli midagi, ei olnud tühi
+			
+			// kas pikkus vähemalt 8
+			if (strlen ($_POST["signupPassword"]) < 8 ) {
+				
+				$signupPasswordError = "Parool peab olema vähemalt 8 tm pikk";
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	$gender = "";
+	if(isset($_POST["gender"])) {
+		if(!empty($_POST["gender"])){
+			
+			//on olemas ja ei ole tühi
+			$gender = $_POST["gender"];
+		}
+	}
+	
+	if ( isset($_POST["signupEmail"]) &&
+		 isset($_POST["signupPassword"]) &&
+		 $signupEmailError == "" &&
+		 empty($signupPasswordError)
+		) {
+			
+			//ühtegi viga ei ole, kõik vajalik olemas
+			echo "salvestan...<br>";
+			echo "email ".$signupEmail."<br>";
+			echo "parool ".$_POST["signupPassword"]."<br>";
+		
+			$password = hash("sha512", "eriti salajane võti");
+			
+			echo "räsi ".$password."<br>";
+			
+			//ühendus
+			$database = "if16_aleksander";
+			$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+			
+			$stmt = $mysqli->prepare("INSERT INTO user_sample 
+			(email, password) VALUES (?, ?)");
+			
+			// s - string
+			// i - int 
+			// d - decimal/double
+			//iga küsimärgi jaoks 1 täht, mis tüüpi on
+			$stmt->bind_param("ss", $signupEmail, $password);
+			
+			//täida käsku
+			if ($stmt->execute() ) {
+				
+				echo "salvestamine õnnestus";
+				
+			} else { 
+			
+				echo "ERROR".$stmt->error;
+			}
+	
+		} 
+	
+	
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST">
+			
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+			
+			<label>Parool</label><br>
+			<input name="loginPassword" type="password">
+						
+			<br><br>
+			
+			<input type="submit">
+		
+		</form>
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+			
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email" value="<?php echo $signupEmail; ?>" > <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			
+			<input placeholder="Parool" name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
+						
+			<br><br>
+			
+			<?php if ($gender == "male") { ?>
+				<input type="radio" name="gender" value="male" checked > Mees<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="male"> Mees<br>
+			<?php } ?>
+			
+			<?php if ($gender == "female") { ?>
+				<input type="radio" name="gender" value="female" checked > Naine<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="female"> Naine<br>
+			<?php } ?>
+			
+			<?php if ($gender == "other") { ?>
+				<input type="radio" name="gender" value="other" checked > Muu<br>
+			<?php } else { ?>
+				<input type="radio" name="gender" value="other"> Muu<br>
+			<?php } ?>
+			
+			<input type="submit" value="Loo kasutaja">
+		
+		</form>
+
+	</body>
+</html>
+
+"MVP idee: Mingi online notepad (nagu google keep v�i evernote)"
