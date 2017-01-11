fdbf9186cea860cb5120b26db0960b31ccd744bf
Kert Aavik <kerta@greeny.cs.tlu.ee>
2017-01-11 10:04:05 +0200

    lisasin uue asja

--- login.php
+++ login.php
@@ -1,78 +1,161 @@
 <?php
-// GET ja POSTi muutujad
-//var_dump ($_GET);
-//echo"<br>";
-//var_dump ($_post);
-$signupEmailError = "";
+	$signupEmailError = "";
 	$signupPasswordError = "";
-//kontrollin, kas epost on olemas
-if (isset ($_POST["signupEmail"])){
-	if (empty ($_POST ["signupEmail"])){
-		$signupEmailError = "See vali on kohustuslik";
-	//Kui email oli tühi
+	$signupTelephoneError = "";
+	$signupUsernameError = "";
+	
+	if (isset ($_POST["signupEmail"])) {
+		
+		if (empty ($_POST["signupEmail"])) {
+		
+		$signupEmailError = "E-post on kohustuslik";
+		
+		}
+			
 	
 	}
-}
-
-	if( isset($_POST["signupPassword"])) {
+	$loginEmailError = "";
+	
+	
+	if (isset ($_POST["loginEmail"])) {
 		
-		if( empty($_POST["signupPassword"])) {
-			$signupPasswordError = "See vali on kohustuslik";	
+		if (empty ($_POST["loginEmail"])) {
+		
+		$loginEmailError = "Vale E-post";
+		
+		}
 			
+	
+	}
+		$signupPasswordError = "";
+	
+	if (isset ($_POST["signupPassword"])) {
 		
-	}else{
-		//Siia jõuan siis, kui parool oli olemas ja parool, ei olnud tühi. !ELSE!
-			if(strlen($_POST["signupPassword"])<8) {
+		if (empty ($_POST["signupPassword"])) {
+		
+		$signupPasswordError = "Parool on kohustuslik";
+		
+		} else {
+			
+			if (strlen ($_POST["signupPassword"]) < 5) {
 				
-				$signupPasswordError = "Parool peab olema vahemalt 8 marki pikk"; 
+				$signupPasswordError = "Parool miinimum 5 tähemärki";
 				
 			}
 			
+		}
 			
-		
-		
+	
 	}
-}
+	if (isset ($_POST["signupUsername"])) {
+		
+		if (empty ($_POST["signupUsername"])) {
+		
+		$signupUsernameError = "Kasutajanimi on kohustuslik";
+		
+		} else {
+			
+			
+			if (strlen ($_POST["signupUsername"]) < 4) {
+				
+				$signupUsernameError = "Miinimum 4 tähemärki";
+			}	
+			
+		}
+			
+	
+	}
+	if (isset ($_POST["signupTelephone"])) {
+		
+		if (empty ($_POST["signupTelephone"])) {
+		
+		$signupTelephoneError = "Telefoninumber on kohustuslik";
+		
+		} else {
+			
+			if (strlen ($_POST["signupTelephone"]) < 7) {
+				
+				$signupUsernameError = "Miinimum 7 tähemärki";
+			}	
+			
+		}
+		
+				
+	}
 	
 
+
+//Mvp ideeks on leht , kus inimesed saavad autosid hinnata riikide järgi autosid, ehk siis inimene tuleb lehele valib listist omale sobivaima auto ja saab selle esitada enda valikuks
 	
 ?>
 
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
-<title>Sisselogimise lehekulg</title>
-<body>
-<!-- sedasi käivad kommentaarid! -->
-<!-- label annab inputile nimetuse -->
-<h1>Logi sisse</h1>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
 
-	<form method = "POST">
+		<h1>Logi Sisse</h1>
 	
-	<label>E-post</label><br>
-	<input name="loginemail" type="email">
-	<br><br>
-	
-	<label>Parool</label><br>
-	<input name="loginpassword" type="password">
-	<br><br>
-	
-	<input type="submit" value="Logi sisse">
-	
-	<h1>Loo kasutaja</h1>
+		<form method="POST">
+			<label>E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+			<label>Parool</label><br>
+			<input name="loginPassword" type="password">
+			
+			<br><br>
+			
+			<input type="submit" value="Logi Sisse" >
+		
+		</form>
 
-	<form method = "POST">
-	
-	<label>E-post</label><br>
-	<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
-	<br><br>
-	
-	<label>Parool</label><br>
-	<input name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
-	<br><br>
-	
-	<input type="submit" value="Registreeru">
-	
-	</form>
-
-</body>
+	</body>
 </html>
+
+
+
+<h1>Loo Kasutaja</h1>
+	
+
+	
+		
+	<html>
+		
+			<form method="POST">
+		
+			<label>Kasutajanimi</label><br>
+			<input name="signupUsername" type="text"> <?php echo $signupUsernameError; ?>
+			
+			<br><br>
+			<label>E-post</label><br>
+			<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			<label>Parool</label><br>
+			<input name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
+			
+			<br><br>
+			<label>Telefon</label><br>
+			<input name="signupTelephone" type="tel"> <?php echo $signupTelephoneError; ?>
+			
+			<br><br>
+			
+			<input type="submit" value="Loo Kasutaja" >
+			
+			
+		
+		</form>
+	
+	</body>
+</html>
+
